3f38eccc798556f59db54a309ab70341a11c44f1
StacyLafume <stacylafume123@gmail.com>
2020-03-27 17:41:11 -0400

    the pink background

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `test`,
+    description: `first Gatsby site`,
+    author: `@stacylafume`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-theme-material-ui`,
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
